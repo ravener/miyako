08bcb2290be517b372a6f38f4e061e2b14fdfb22
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-06-09 23:04:55 +0300

    fix

--- cogs/owner.py
+++ cogs/owner.py
@@ -11,15 +11,16 @@ class Owner:
     async def sudo(self, ctx, user: discord.Member, command: str, *, args = None):
         """Calls a command on behalf of another person"""
         arg_dict = {}
-        arr = args.split(",")
-        for x in arr:
-            split = x.split(":")
-            name = split[0]
-            value = split[1]
-            arg_dict[name] = value
+        if args:
+            arr = args.split(",")
+            for x in arr:
+                split = x.split(":")
+                name = split[0]
+                value = split[1]
+                arg_dict[name] = value
             
         ctx.author = user
-        cmd = bot.get_command(command)
+        cmd = self.bot.get_command(command)
         if not cmd:
             return await ctx.send(f"Command {command} does not exist.")
         if len(args_dict) > 0:
