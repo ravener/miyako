694cb419c7e5d3b61902a9f4a4057082bc2958c1
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-06-09 22:44:15 +0300

    i broke ctx?

--- ext/context.py
+++ ext/context.py
@@ -2,7 +2,6 @@ import discord
 from discord.ext import commands
 
 class Context(commands.Context):
-    def __init__(*args, **kwargs):
-        super().__init__(*args, **kwargs)
+    pass
     
     # TODO
