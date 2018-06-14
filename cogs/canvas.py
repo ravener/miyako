23a962fe9e3e2f5891e1b849676bb04962b66adc
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-06-14 17:06:08 +0300

    Stuff

--- cogs/canvas.py
+++ cogs/canvas.py
@@ -7,6 +7,11 @@ class Canvas:
     def __init__(self, bot):
         self.bot = bot
         self.client = idioticapi.Client(os.environ.get("IDIOTICAPI"), dev=True)
+     
+    def av(self, user):
+        # is this a d.py bug? size 2048 isn't allowed.
+        # while it is a valid size for avatars.
+        return user.avatar_url_as(format="png", size=1024).replace("1024", "2048")
         
     @commands.command()
     @commands.cooldown(1, 5, commands.BucketType.user)
@@ -14,7 +19,7 @@ class Canvas:
         if not user:
             user = ctx.author
         try:
-            await ctx.send(file=discord.File(await self.client.triggered(user.avatar_url_as(format="png", size=2048)), "triggered.gif"))
+            await ctx.send(file=discord.File(await self.client.triggered(self.av(user)), "triggered.gif"))
         except Exception as e:
             await ctx.send(f"Something went wrong, please try again later.```{e}```")
             print(e)
--- utils/utils.py
+++ utils/utils.py
@@ -13,3 +13,16 @@ def capitialize(msg: str):
     for x in snakes:
         res.append(x.title())
     return " ".join(res)
+
+def paginate(text: str):
+    """Simple generator that paginates text."""
+    last = 0
+    pages = []
+    for curr in range(0, len(text)):
+        if curr % 1980 == 0:
+            pages.append(text[last:curr])
+            last = curr
+            appd_index = curr
+    if appd_index != len(text) - 1:
+        pages.append(text[last:curr])
+    return list(filter(lambda a: a != '', pages))
