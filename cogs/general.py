9cbb56af2f064b89199e669208d4ec07aafbc782
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-06-16 16:43:26 +0300

    smh

--- cogs/general.py
+++ cogs/general.py
@@ -5,7 +5,7 @@ class General:
     """Some general commands."""
     def __init__(self, bot):
         self.bot = bot
-
+    
     @commands.command()
     async def say(self, ctx, *, msg: commands.clean_content()):
         """Bot repeats what you say."""
@@ -25,7 +25,7 @@ class General:
     async def invite(self, ctx):
         """Want me in your server?"""
         await ctx.send(f"Invite me to your server: https://discordapp.com/oauth2/authorize?client_id={self.bot.user.id}&scope=bot&permissions=470281463")
-
+    
 
 def setup(bot):
     bot.add_cog(General(bot))
