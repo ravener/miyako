7e94ec77212317b61465a32ff0af5aa0adb54eaf
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-06-25 16:49:05 +0300

    Suggestion cmd

--- cogs/general.py
+++ cogs/general.py
@@ -1,12 +1,11 @@
 import discord
 from discord.ext import commands
-from ext.paginator import Paginator
 
 class General:
     """Some general commands."""
     def __init__(self, bot):
         self.bot = bot
-
+    
     @commands.command()
     async def say(self, ctx, *, msg: commands.clean_content()):
         """Bot repeats what you say."""
@@ -26,47 +25,22 @@ class General:
     async def invite(self, ctx):
         """Want me in your server?"""
         await ctx.send(f"Invite me to your server: https://discordapp.com/oauth2/authorize?client_id={self.bot.user.id}&scope=bot&permissions=470281463")
-
-    @commands.command(name="help", aliases=["h", "halp", "commands", "cmds"])
-    async def _help(self, ctx, command: str = None):
-        if command:
-            cmd = self.bot.get_command(command.lower()) or self.bot.get_cog(command)
-            if not cmd:
-                return await ctx.send(f"Command or category '{command}' not found.")
-            if isinstance(cmd, commands.Command):
-                em = discord.Embed(color=0xff0000)
-                em.title = cmd.name
-                em.description = cmd.help or "No Description"
-                em.description += "\nUsage: {}{}".format(ctx.prefix, cmd.signature)
-                return await ctx.send(embed=em)
-            cmds = self.bot.get_cog_commands(command)
-            em = discord.Embed(color=0xff0000)
-            em.description = cmd.__doc__ + "\n\n`" if cmd.__doc__ else "No Description\n\n`"
-            em.set_footer(text=f"{ctx.prefix}help <cmd> for more info on a command.")
-            for x in cmds:
-                msg = f"{ctx.prefix}{x.signature} {x.short_doc}\n"
-                em.description += msg
-            em.description += "`"
-            return await ctx.send(embed=em)
-        else:
-            cogs = self.bot.cogs.keys()
-            pages = []
-            for x in cogs:
-                if x == "Owner" and ctx.author.id != 292690616285134850:
-                    continue
-                cmds = self.bot.get_cog_commands(x)
-                cog = self.bot.get_cog(x)
-                msg = cog.__doc__ + "\n\n`" if cog.__doc__ else "No Description\n\n`"
-                for cmd in cmds:
-                    cmd_msg = f"{ctx.prefix}{cmd.signature} {cmd.short_doc}\n"
-                    msg += cmd_msg
-                msg += "`"
-                pages.append(msg)
-            em = discord.Embed(color=0xff0000)
-            em.set_footer(text=f"{ctx.prefix}help <cmd> for more information on a command.")
-            paginator = Paginator(ctx, pages=pages, page_count=True, embed=em)
-            await paginator.run()
-
+    
+    @commands.command()
+    @commands.cooldown(1, 60, commands.BucketType.user)
+    async def suggest(self, ctx, *, idea: str):
+        channel = self.bot.get_channel(460801007769616394)
+        if len(idea) > 1024:
+            return await ctx.send("Idea too long, please keep it below 1024 chars.")
+        em = discord.Embed(color=0xff0000)
+        em.title = "New Suggestion"
+        em.description = idea
+        em.set_footer(text=f"User ID: {ctx.author.id}")
+        em.set_thumbnail(url=ctx.author.avatar_url)
+        msg = await channel.send(embed=em)
+        await msg.add_reaction("✅")
+        await msg.add_reaction("❌")
+        await ctx.send("You idea has been successfully submitted to the support server, we will reply to you in DM if we have any feedback.")
 
 def setup(bot):
     bot.add_cog(General(bot))
