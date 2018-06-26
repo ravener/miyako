627a1280f5dca404a16e8732dc9fbd6fc1454aed
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-06-26 21:27:23 +0300

    Typing.

--- cogs/fun.py
+++ cogs/fun.py
@@ -82,15 +82,16 @@ class Fun:
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def fml(self, ctx):
         try:
-            res = await self.bot.session.get("http://www.fmylife.com/random")
-            html = await res.text()
-            data = BeautifulSoup(html, "lxml")
-            article = data.find_all("p", { "class": "block" })[0].text
-            em = discord.Embed(color=0xff0000)
-            em.title = "Fuck My Life"
-            em.description = article
-            em.set_footer(text=f"Requested by: {ctx.author}")
-            await ctx.send(embed=em)
+            async with ctx.typing():
+                res = await self.bot.session.get("http://www.fmylife.com/random")
+                html = await res.text()
+                data = BeautifulSoup(html, "lxml")
+                article = data.find_all("p", { "class": "block" })[0].text
+                em = discord.Embed(color=0xff0000)
+                em.title = "Fuck My Life"
+                em.description = article
+                em.set_footer(text=f"Requested by: {ctx.author}")
+                await ctx.send(embed=em)
         except Exception as e:
             await ctx.send("Something went wrong, please try again later.")
             raise e
