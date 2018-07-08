abc4dad8b5130e62341e945a6a80b372ee4a90ce
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-07-09 02:28:42 +0300

    Prefix limit

--- cogs/config.py
+++ cogs/config.py
@@ -12,6 +12,8 @@ class Config:
     @commands.has_permissions(manage_guild=True)
     async def prefix(self, ctx, *, prefix: str):
         """Changes the command prefix for your server"""
+        if len(prefix) > 5:
+            return await ctx.send("Prefix too long, please do not exceed 5 characters.")
         try:
             await self.bot.db.config.update_one({ "_id": ctx.guild.id }, { "$set": { "prefix": prefix } }, upsert=True)
             await ctx.send(f"Changed prefix to `{prefix}` successfully")
