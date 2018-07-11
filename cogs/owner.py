ac8387a31df68cc6481a53728eec800831a48c18
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-07-11 22:39:40 +0300

    repl

--- cogs/owner.py
+++ cogs/owner.py
@@ -16,6 +16,7 @@ class Owner:
     """Core class for owner commands"""
     def __init__(self, bot):
         self.bot = bot
+        self.session = set()
         
     def cleanup_code(self, content):
         '''Automatically removes code blocks from the code.'''
@@ -207,6 +208,96 @@ class Owner:
         if not cmd:
             return await ctx.send(f"Command `{command}` not found.")
         await ctx.paginate(inspect.getsource(cmd.callback), "```py\n", "\n```")
+
+    @commands.command()
+    @commands.is_owner()
+    async def repl(self, ctx):
+        """Launches an interactive REPL session."""
+        variables = {
+            "ctx": ctx,
+            "bot": self.bot,
+            "message": ctx.message,
+            "guild": ctx.guild,
+            "channel": ctx.channel,
+            "author": ctx.author,
+            "_": None,
+            "session": self.bot.session,
+            "source": inspect.getsource,
+            "src": inspect.getsource
+        }
+
+        if ctx.channel.id in self.sessions:
+            await ctx.send('Already running a REPL session in this channel. Exit it with `quit`.')
+            return
+
+        self.sessions.add(ctx.channel.id)
+        await ctx.send("Enter code to execute or evaluate. `exit()` or `quit` to exit.")
+
+        def check(m):
+            return m.author.id == ctx.author.id and \
+                   m.channel.id == ctx.channel.id
+
+        while True:
+            try:
+                response = await self.bot.wait_for("message", check=check, timeout=10.0 * 60.0)
+            except asyncio.TimeoutError:
+                await ctx.send("Exiting REPL session.")
+                self.sessions.remove(ctx.channel.id)
+                break
+
+            cleaned = self.cleanup_code(response.content)
+
+            if cleaned in ("quit", "exit", "exit()"):
+                await ctx.send("Exiting.")
+                self.sessions.remove(ctx.channel.id)
+                return
+
+            executor = exec
+            if cleaned.count("\n") == 0:
+                # single statement, potentially 'eval'
+                try:
+                    code = compile(cleaned, "<repl session>", "eval")
+                except SyntaxError:
+                    pass
+                else:
+                    executor = eval
+
+            if executor is exec:
+                try:
+                    code = compile(cleaned, "<repl session>", "exec")
+                except SyntaxError as e:
+                    await ctx.send(f"```\n{traceback.format_exc()}\n```")
+                    continue
+
+            variables["message"] = response
+
+            fmt = None
+            stdout = io.StringIO()
+
+            try:
+                with redirect_stdout(stdout):
+                    result = executor(code, variables)
+                    if inspect.isawaitable(result):
+                        result = await result
+            except Exception as e:
+                value = stdout.getvalue()
+                fmt = f"```py\n{value}{traceback.format_exc()}\n```"
+            else:
+                value = stdout.getvalue()
+                if result is not None:
+                    fmt = f"```py\n{value}{result}\n```"
+                    variables["_"] = result
+                elif value:
+                    fmt = f"```py\n{value}\n```"
+
+            try:
+                if fmt is not None:
+                    await ctx.paginate(fmt)
+            except discord.Forbidden:
+                pass
+            except discord.HTTPException as e:
+                await ctx.send(f"Unexpected error: `{e}`")
+
 
 
 def setup(bot):
