04c2629b8abb40f7bbd50303654b1e4e5c42d08b
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-08-11 20:43:34 +0300

    semicolon

--- commands/Moderation/hackban.js
+++ commands/Moderation/hackban.js
@@ -14,7 +14,7 @@ class Hackban extends Command {
   
   async run(msg, [id]) {
     if(isNaN(parseInt(id))) throw "Invalid user id.";
-    if(msg.guild.members.has(id)) throw `That user is in the server, hackban is meant to ban people who isn't in the server to prevent them from (re)joining in the future, use \`${msg.guild.settings.prefix}ban\` for that user.`
+    if(msg.guild.members.has(id)) throw `That user is in the server, hackban is meant to ban people who isn't in the server to prevent them from (re)joining in the future, use \`${msg.guild.settings.prefix}ban\` for that user.`;
     return msg.guild.members.ban(id).then((res) => {
       return msg.send(`Hackbanned **${res.tag}**`);
     }).catch(() => msg.send("Couldn't ban that user, make sure the ID is valid."));
