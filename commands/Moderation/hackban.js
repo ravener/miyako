665075e147dbe092c2281db5af2bda3d08e7db8a
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-08-11 20:39:43 +0300

    hackban

---
+++ commands/Moderation/hackban.js
@@ -0,0 +1,24 @@
+const { Command } = require("klasa");
+
+class Hackban extends Command {
+  constructor(...args) {
+    super(...args, {
+      description: "Bans a user that isn't in the server.",
+      permissionLevel: 5,
+      aliases: ["hban"],
+      requiredPermissions: ["BAN_MEMBERS"],
+      usage: "<id:string>",
+      runIn: ["text"]
+    });
+  }
+  
+  async run(msg, [id]) {
+    if(isNaN(parseInt(id))) throw "Invalid user id.";
+    if(msg.guild.members.has(id)) throw `That user is in the server, hackban is meant to ban people who isn't in the server to prevent them from (re)joining in the future, use \`${msg.guild.settings.prefix}ban\` for that user.`
+    return msg.guild.members.ban(id).then((res) => {
+      return msg.send(`Hackbanned **${res.tag}**`);
+    }).catch(() => msg.send("Couldn't ban that user, make sure the ID is valid."));
+  }
+}
+
+module.exports = Hackban;
