279295dbc716e51bbf32756ffe636c516941c55b
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-08-14 20:03:25 +0300

    fix

--- commands/Canvas/achievement.js
+++ commands/Canvas/achievement.js
@@ -6,12 +6,13 @@ class Achievement extends Command {
     super(...args, {
       description: "Get an achievement!",
       cooldown: 5,
-      usage: "[user:username] <text:string{,22}> [...]",
+      usage: "[user:username] <text:string> [...]",
       usageDelim: " "
     });
   }
 
   async run(msg, [user = msg.author, ...text]) {
+    if(text.join(" ").length > 22) throw "Text must be smaller than 22 characters.";
     return msg.send(new MessageAttachment(await this.client.idioticapi.achievement(user.displayAvatarURL({ format: "png", size: 2048 }), text.join(" ")), "achievement.png"));
   }
 }
