1b732110933fb9816bbca7d74d965bac67a96b98
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-08-14 18:19:16 +0300

    better?

--- events/commandUnknown.js
+++ events/commandUnknown.js
@@ -3,9 +3,7 @@ const { Event } = require("klasa");
 class CommandUnknown extends Event {
   
   async run(msg, cmd) {
-    const tag = msg.guild.settings.tags.find((x) => x.name === cmd.toLowerCase());
-    if(!tag) return;
-    msg.send(tag.content.replace(/@(everyone|here)/g, "@\u200b$1"));
+    this.client.commands.get("tag").get(msg, cmd.toLowerCase()).catch(() => null);
   }
 }
 
