10d81a43831f887f509c276a2f6c6a54a3aeccb7
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-08-14 19:50:26 +0300

    more cmds

---
+++ commands/Canvas/sniper.js
@@ -0,0 +1,18 @@
+const { Command } = require("klasa");
+const { MessageAttachment } = require("discord.js");
+
+class Sniper extends Command {
+  constructor(...args) {
+    super(...args, {
+      description: "Snipe someone",
+      usage: "[user:username]",
+      cooldown: 5
+    });
+  }
+
+  async run(msg, [user = msg.author]) {
+    return msg.send(new Attachment(await this.client.idioticapi.sniper(user.displayAvatarURL({ format: "png", size: 2048 })), "sniper.png"));
+  }
+}
+
+module.exports = Sniper;
