414683c2786c26af2465eb19bdeae409438da753
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-08-17 20:47:08 +0300

    fix?

--- commands/Misc/snipe.js
+++ commands/Misc/snipe.js
@@ -13,15 +13,14 @@ class Snipe extends Command {
   
   async run(msg, [channel = msg.channel]) {
     if(!channel.snipe) throw `There isn't any sniped message in ${channel === msg.channel ? "this" : "that"} channel.`;
-    const attach = getAttachment(channel.snipe);
     const embed = new MessageEmbed()
       .setTitle("Message Sniped")
       .setDescription(channel.snipe.content)
       .setAuthor(channel.snipe.author.tag, channel.snipe.author.displayAvatarURL())
       .setFooter(`Sent by ${channel.snipe.author.tag}`)
       .setTimestamp(channel.snipe.createdAt)
+      .setImage(getAttachment(channel.snipe))
       .setColor(0xff0000);
-    if(attach) embed.setImage(attach);
     return msg.send({ embed });
   }
 }
