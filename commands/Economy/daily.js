2279211908c8257ea23e84002465ba6c69384395
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-08-19 21:14:12 +0300

    fix

--- commands/Economy/daily.js
+++ commands/Economy/daily.js
@@ -9,7 +9,7 @@ class Daily extends Command {
   }
 
   async run(msg) {
-    if(Date.now() < msg.member.settings.daily) throw `You can claim your daily in **${Duration.fromNow(msg.member.settings.daily)}**`;
+    if(Date.now() < msg.member.settings.daily) throw `You can claim your daily in **${Duration.toNow(msg.member.settings.daily)}**`;
     await msg.member.settings.update("daily", msg.createdTimestamp + 86400000);
     await msg.member.givePoints(500);
     return msg.send("You've collected your daily **$500**! Come back tomorrow for more.");
