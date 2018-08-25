8cadb90940e99183255b549bb77051aff6ebed0d
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-08-26 02:25:01 +0300

    lol

---
+++ commands/Canvas/magik.js
@@ -0,0 +1,25 @@
+const { Command } = require("klasa");
+const { get } = require("superagent");
+const { MessageEmbed } = require("discord.js");
+
+class Magik extends Command {
+  constructor(...args) {
+    super(...args, {
+      description: "Magik someone",
+      usage: "[user:username]",
+      cooldown: 5
+    });
+  }
+
+  async run(msg, [user = msg.author]) {
+    const res = await get("https://nekobot.xyz/api/imagegen")
+      .query({ type: "magik", image: user.displayAvatarURL({ format: "png", size: 2048 }) });
+    const embed = new MessageEmbed()
+      .setAuthor(user.tag, user.displayAvatarURL())
+      .setColor(0xff0000)
+      .setImage(res.message);
+    return msg.send({ embed });
+  }
+}
+
+module.exports = Magik;
