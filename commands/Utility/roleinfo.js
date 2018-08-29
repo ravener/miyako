53bddb7e32605d189d8b0e1fd6daf6cb378f09f2
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-08-30 01:05:29 +0300

    stuff

---
+++ commands/Utility/roleinfo.js
@@ -0,0 +1,37 @@
+const { Command, Timestamp } = require("klasa");
+const { capitalize } = require("../../utils/utils.js");
+const { MessageEmbed } = require("discord.js");
+
+class RoleInfo extends Command {
+  constructor(...args) {
+    super(...args, {
+      runIn: ["text"],
+      description: "Get information on a role with an id or a mention.",
+      usage: "<role:rolename>"
+    });
+    
+    this.perms = {
+      MANAGE_GUILD: "Manage Server",
+      VIEW_CHANNEL: "Read Text Channels and See Voice Channels",
+      USE_VAD: "Use Voice Activity"
+    };
+    this.timestamp = new Timestamp("MMMM dd YYYY");
+  }
+  
+  run(msg, [role]) {
+    const allPermissions = Object.entries(role.permissions.serialize()).filter(perm => perm[1]).map(([perm]) => this.perms[perm] ? this.perms[perm] : capitialize(perm)).join(", ");
+    
+    const embed = new MessageEmbed()
+      .setColor(role.hexColor || 0xff0000)
+      .addField("❯ Name", role.name, true)
+      .addField("❯ ID", role.id, true)
+      .addField("❯ Color", role.hexColor || "None", true)
+      .addField("❯ Creation Date", this.timestamp.display(role.createdAt), true)
+      .addField("❯ Hoisted", role.hoist ? "Yes" : "No", true)
+      .addField("❯ Mentionable", role.mentionable ? "Yes" : "No", true)
+      .addField("❯ Permissions", allPermissions);
+    return msg.send({ embed });
+  }
+}
+
+module.exports = RoleInfo;
---
+++ commands/Utility/wikipedia.js
@@ -0,0 +1,31 @@
+const { Command } = require("klasa");
+const { MessageEmbed } = require("discord.js");
+const fetch = require("node-fetch");
+
+class Wikipedia extends Command {
+  constructor(...args) {
+    super(...args, {
+      aliases: ["wiki"],
+      description: "Finds a Wikipedia Article by title.",
+      usage: "<query:string>"
+    });
+  }
+  
+  async run(msg, [query]) {
+    const article = await fetch(`https://en.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(query)}`)
+      .then(response => response.json())
+      .catch(() => {
+        throw "I couldn't find a wikipedia article with that title!";
+      });
+      
+    const embed = new MessageEmbed()
+      .setColor(4886754)
+      .setThumbnail((article.thumbnail && article.thumbnail.source) || "https://i.imgur.com/fnhlGh5.png")
+      .setURL(article.content_urls.desktop.page)
+      .setTitle(article.title)
+      .setDescription(article.extract);
+    return msg.send({ embed });
+  }
+}
+
+module.exports = Wikipedia;
