00f488b8d915942207f068573da1640008a8ff8e
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-09-01 19:19:06 +0300

    Turn off afk after 3 messages

--- monitors/afk.js
+++ monitors/afk.js
@@ -6,23 +6,22 @@ class AFK extends Monitor {
     super(...args, {
       ignoreOthers: false
     });
+
+    this.counter = new Map();
   }
   
-  run(message) {
+  async run(message) {
     if(!message.guild) return;
+    if(message.author.settings.afk.status) this.counter.set(message.author.id, (this.counter.get(message.author.id) || 0) + 1);
+    if(message.author.settings.afk.status && this.counter.get(message.author.id) > 2) {
+      await message.author.settings.update("afk.status", false);
+      return message.channel.send(`**${message.member.displayName}** welcome back, i've turned off your afk status`);
+    }
     if(message.mentions.users.size) {
       const afks = message.mentions.users.filter((x) => x.settings.afk.status && x.settings.afk.message && x.id !== message.author.id);
       if(!afks.size) return;
       return message.send(afks.map((x) => `**${x.username}** is AFK, message: \`${escapeMarkdown(x.settings.afk.message, true)}\`\n`));
     } 
-  }
-  
-  async init() {
-    const { schema } = this.client.gateways.users;
-    if(!schema.has("afk")) await schema.add("afk", {
-      status: { type: "boolean" },
-      message: { type: "string" }
-    });
   }
 }
 
