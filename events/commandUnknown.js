c484e07149a1845c969f0191b1475dfa6649d445
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-09-07 21:47:09 +0300

    fix

--- events/commandUnknown.js
+++ events/commandUnknown.js
@@ -6,19 +6,26 @@ class CommandUnknown extends Event {
     super(...args);
     this.bot = new Cleverbot(this.client.config.cleverbot.user, this.client.config.cleverbot.key);
     this.bot.setNick("Ladybug");
+    this.mention = null;
   }
 
   async run(msg, cmd) {
     const res = await this.client.commands.get("tag").get(msg, cmd.toLowerCase()).catch(() => false);
     if(res) return;
     // CleverBot
-    if(msg.prefix !== this.client.options.regexPrefix && msg.prefix.toString() !== `/^<@!?${this.client.user.id}>/`) return; // Only work with mention and regex prefix
+    const match = this.mention.exec(msg.content) || this.client.options.regexPrefix.exec(msg.content);
+    if(!match) return;
+    const prefix = match[0];
     await msg.channel.startTyping();
-    const text = msg.content.slice(msg.prefixLength).trim();
+    const text = msg.content.slice(prefix.length).trim();
     return this.bot.ask(text, (err, res) => {
       if(err) throw err;
       return msg.send(res);
     });
+  }
+
+  async init() {
+    this.mention = new RegExp(`^<@!?${this.client.user.id}>`);
   }
 }
 
