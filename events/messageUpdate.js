eedd26042c0dd4a50d180b6a7d4430916dbf9a60
SharpBit <31069084+SharpBit@users.noreply.github.com>
2018-09-08 08:26:16 -0400

    make the check below the monitor as requested

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -4,8 +4,8 @@ class MessageUpdate extends Event {
   
   async run(old, message) {
     if (old.content === message.content) return;
-    if (message.author.bot) return;
     if (this.client.ready) this.client.monitors.run(message);
+    if (message.author.bot) return;
     this.client.emit("modlogs", message.guild, "messageUpdate", { old, message, name: "messages" });
   }
 }
