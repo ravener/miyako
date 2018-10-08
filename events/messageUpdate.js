e562954d78ac5fcf398c6bc58187bfb652782fab
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-10-08 21:15:15 +0300

    xd

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -5,6 +5,7 @@ class MessageUpdate extends Event {
   async run(old, message) {
     if (old.content === message.content) return;
     if (this.client.ready) this.client.monitors.run(message);
+    if(message.channel.type === "text" && message.author.id !== this.client.user.id) message.channel.editSnipe = { old, message };
     if (message.author.bot) return;
     this.client.emit("modlogs", message.guild, "messageUpdate", { old, message, name: "messages" });
   }
