c0be6ea0860d5e87662d5302ae58c9d8e3e4f1d9
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-10-15 19:53:12 +0300

    enable/disable commands

---
+++ commands/Owner/disablepiece.js
@@ -0,0 +1,27 @@
+const { Command } = require("klasa");
+
+class Disable extends Command {
+  constructor(...args) {
+    super(...args, {
+      permissionLevel: 10,
+      guarded: true,
+      description: language => language.get("COMMAND_DISABLE_DESCRIPTION"),
+      usage: "<Piece:piece>"
+    });
+  }
+
+  async run(message, [piece]) {
+    if ((piece.type === "event" && piece.name === "message") || (piece.type === "monitor" && piece.name === "commandHandler")) {
+      return message.sendLocale("COMMAND_DISABLE_WARN");
+    }
+    piece.disable();
+    if (this.client.shard) {
+      await this.client.shard.broadcastEval(`
+				if (this.shard.id !== ${this.client.shard.id}) this.${piece.store}.get('${piece.name}').disable();
+			`);
+    }
+    return message.sendLocale("COMMAND_DISABLE", [piece.type, piece.name], { code: "diff" });
+  }
+}
+
+module.exports = Disable;
