48927e681ea1e0cedb61d157d1dd0a90bb2aa10e
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-10-15 19:17:55 +0300

    fix help

--- commands/General/Chat Bot Info/help.js
+++ commands/General/Chat Bot Info/help.js
@@ -71,9 +71,9 @@ class Help extends Command {
       for(let subCat = 0; subCat < subCategories.length; subCat++) helpMessage.push(`= ${subCategories[subCat]} =`, `${help[categories[cat]][subCategories[subCat]].join("\n")}\n`);
     }
     helpMessage.push("```", "\u200b");
-    return msg.author.send(helpMessage, { split: { char: "\u200b" } })
+    return msg.author.send(helpMessage, { split: "\u200b" })
       .then(() => { if(msg.channel.type !== "dm") msg.sendLocale("COMMAND_HELP_DM"); })
-      .catch((e) => { throw e; if(msg.channel.type !== "dm") msg.sendLocale("COMMAND_HELP_NODM"); });
+      .catch(() => { if(msg.channel.type !== "dm") msg.sendLocale("COMMAND_HELP_NODM"); });
   }
   
   async buildHelp(message) {
