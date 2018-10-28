57134b81e630be839e5edec0e440b6920a09e5b0
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-10-28 18:41:04 +0300

    new leaderboard

--- commands/Social/leaderboard.js
+++ commands/Social/leaderboard.js
@@ -1,4 +1,6 @@
+/* eslint-disable */
 const { Command, util: { codeBlock } } = require("klasa");
+const { MessageEmbed } = require("discord.js");
 
 class Leaderboard extends Command {
   constructor(...args) {
@@ -6,7 +8,8 @@ class Leaderboard extends Command {
       description: "View the top leaderboard",
       runIn: ["text"],
       usage: "[page:int]",
-      aliases: ["lb", "top", "top10", "leader"]
+      aliases: ["lb", "top", "top10", "leader"],
+      requiredPermissions: ["EMBED_LINKS"]
     });
   }
 
@@ -19,17 +22,21 @@ class Leaderboard extends Command {
     const total = Math.round(list.length / 10);
     if(total === 0) throw "Couldn't get top 10 members, maybe it's a dead place?";
     if(page + 1 > total) throw `There are only **${total || 1}** pages in the leaderboard`;
+    const { currency } = this.client.constants;
+    const embed = new MessageEmbed()
+      .setColor(0xff0000)
+      .setTitle("Leaderboard")
+      .setAuthor(msg.author.tag, msg.author.displayAvatarURL());
     const top = list.slice(page * 10, (page + 1) * 10);
     const leaderboard = [];
     const numPos = list.indexOf(msg.member);
-    const pos = numPos === -1 ? "??" : (list.indexOf(msg.member) + 1).toString().padStart(2, "0");
+    const pos = numPos === -1 ? "??" : (list.indexOf(msg.member) + 1).toString();
     top.map((x, i) => {
-      leaderboard.push(`${(page * 10 + (i + 1)).toString().padStart(2, "0")} ❯ ${x.user.tag}${" ".repeat(35 - x.user.tag.length)}::  ${x.settings.points.toLocaleString()}`);
+      leaderboard.push(`${(page * 10 + (i + 1)).toString()} ${x.user.username}: ${x.settings.points.toLocaleString()} ${currency}`);
     });
-    leaderboard.push("-".repeat(50));
-    leaderboard.push("Your Position:");
-    leaderboard.push(`${pos} ❯ ${msg.author.tag}${" ".repeat(35 - msg.author.tag.length)}::  ${msg.member.settings.points.toLocaleString()}`);
-    return msg.send(`**Top Leaderboard for __${msg.guild.name}__ (Page ${page + 1} out of ${total || 1})**${codeBlock("ascii", leaderboard.join("\n"))}`);
+    embed.setDescription(leaderboard.join("\n"));
+    embed.setFooter(`Your Position: ${pos}`, msg.guild.iconURL());
+    return msg.send(`**Top Leaderboard for __${msg.guild.name}__ (Page ${page + 1} out of ${total || 1})**`, { embed });
   }
 }
 
