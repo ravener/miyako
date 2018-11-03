acca3dc6398adf5355d5250b975a90471288c097
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-11-04 01:34:02 +0300

    fix.

--- stores/FunctionStore.js
+++ stores/FunctionStore.js
@@ -6,6 +6,18 @@ class FunctionStore extends Store {
     super(client, "functions", LadybugFunction);
   }
 
+  set(key, value) {
+    super.set(key, value);
+    this[key] = value.run.bind(value);
+    for(const name of value.aliases) {
+      Object.defineProperty(this, name, {
+        get: () => {
+          return this.get(value.name).run.bind(value);
+        }
+      });
+    }
+  }
+
   async init() {
     await super.init();
     for(const [key, value] of this.entries()) {
