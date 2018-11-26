cf025d435631357984b3625d19e1282607f1335e
pollen5 <mohammed.ahmed.minecraft@gmail.com>
2018-11-26 17:48:50 +0300

    lol

---
+++ commands/Misc/mimic.js
@@ -0,0 +1,26 @@
+const { Command } = require("klasa");
+
+class Mimic extends Command {
+  constructor(...args) {
+    super(...args, {
+      description: "Copy someone and talk as them.",
+      aliases: ["copycat"],
+      requiredPermissions: ["MANAGE_WEBHOOKS"],
+      usage: "<member:member|user:user> <message:...string>",
+      usageDelim: " ",
+      runIn: ["text"],
+      permissionLevel: 4
+    });
+  }
+
+  async run(msg, [user, message]) {
+    if(msg.deletable) await msg.delete().catch(() => null);
+    const name = user.displayName || user.username;
+    const avatar = user.user ? user.user.displayAvatarURL({ format: "png", size: 2048 }) : user.displayAvatarURL({ format: "png", size: 2048 });
+    const webhook = await msg.channel.createWebhook(name, { avatar });
+    await webhook.send(message);
+    await webhook.delete();
+  }
+}
+
+module.exports = Mimic;
