4f26a5cc8f602ebeadf9dd229f9cd16c796913b0
pollen5 <mohammed.ahmed.minecraft@gmail.com>
2018-11-26 20:30:14 +0300

    klasa breaking change

--- utils/permissionLevels.js
+++ utils/permissionLevels.js
@@ -1,10 +1,11 @@
 const { PermissionLevels } = require("klasa");
 
-const perms = new PermissionLevels()
+module.exports = new PermissionLevels()
   // User
   .add(0, () => true)
   // Beta testers
-  .add(2, (client, msg) => {
+  .add(2, (msg) => {
+    const { client } = msg;
     const guild = client.guilds.get(client.constants.mainGuild);
     if(!guild) return false;
     const member = guild.members.get(msg.author.id);
@@ -12,7 +13,8 @@ const perms = new PermissionLevels()
     return member.roles.has(client.constants.betaRole);
   }, { fetch: true, break: true })
   // Premium users
-  .add(3, (client, msg) => {
+  .add(3, (msg) => {
+    const { client } = msg;
     const guild = client.guilds.get(client.constants.mainGuild);
     if(!guild) return false;
     const member = guild.members.get(msg.author.id);
@@ -20,17 +22,15 @@ const perms = new PermissionLevels()
     return member.roles.has(client.constants.premiumRole);
   }, { fetch: true, break: true })
   // Mini mods
-  .add(4, (client, message) => message.guild && message.member.permissions.has("MANAGE_MESSAGES"), { fetch: true })
+  .add(4, (message) => message.guild && message.member.permissions.has("MANAGE_MESSAGES"), { fetch: true })
   // Moderators
-  .add(5, (client, message) => message.guild && (message.member.permissions.has("BAN_MEMBERS") && message.member.permissions.has("KICK_MEMBERS")), { fetch: true })
+  .add(5, (message) => message.guild && (message.member.permissions.has("BAN_MEMBERS") && message.member.permissions.has("KICK_MEMBERS")), { fetch: true })
   // Server managers.
-  .add(6, (client, message) => message.guild && message.member.permissions.has("MANAGE_GUILD"), { fetch: true })
+  .add(6, (message) => message.guild && message.member.permissions.has("MANAGE_GUILD"), { fetch: true })
   // Admins.
-  .add(7, (client, message) => message.guild && message.member.permissions.has("ADMINISTRATOR"), { fetch: true })
+  .add(7, (message) => message.guild && message.member.permissions.has("ADMINISTRATOR"), { fetch: true })
   // Guild Owner.
-  .add(8, (client, message) => message.guild && message.member === message.guild.owner, { fetch: true })
+  .add(8, (message) => message.guild && message.member === message.guild.owner, { fetch: true })
   // Bot Owner.
-  .add(9, (client, message) => message.author === client.owner, { break: true })
-  .add(10, (client, message) => message.author === client.owner);
-
-module.exports = perms;
+  .add(9, ({ author, client }) => author === client.owner, { break: true })
+  .add(10, ({ author, client }) => author === client.owner);
