ecef893bc7f4132e73dd816daa503ef203d434e0
pollen5 <mohammed.ahmed.minecraft@gmail.com>
2018-12-14 01:59:18 +0300

    wikia command

---
+++ commands/Utility/wikia.js
@@ -0,0 +1,31 @@
+const { Command, RichDisplay } = require("klasa");
+const { MessageEmbed } = require("discord.js");
+const Wikia = require("wikia");
+
+class Wikia extends Command {
+  constructor(...args) {
+    super(...args, {
+      description: "Search a wiki from wikia.com",
+      usage: "<term:string>",
+      requiredPermissions: ["MANAGE_MESSAGES", "ADD_REACTIONS", "READ_MESSAGE_HISTORY", "EMBED_LINKS"]
+    });
+    this.wikia = new Wikia();
+  }
+
+  async run(msg, [term]) {
+    const res = await this.wikia.searchWikis(term).catch(() => null);
+    if(!res || !res.items || !res.items.length) throw "No results found.";
+    const display = new RichDisplay(new MessageEmbed().setColor(0xFF0000));
+    for(const wiki of res) display.addPage((em) => em
+      .setTitle(wiki.name)
+      .setURL(wiki.url)
+      .setDescription(`**${wiki.headline || ""}**\n\n${wiki.desc}\n\n${wiki.url}`)
+      .setImage(wiki.image));
+    return display.run(await msg.send("Loading..."), {
+      filter: (reaction, user) => user.id === msg.author.id,
+      time: 30 * 1000
+    });
+  }
+}
+
+module.exports = Wikia;
