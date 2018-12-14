a4fa100726939736d49ed4f63a981d378469e02b
pollen5 <mohammed.ahmed.minecraft@gmail.com>
2018-12-14 20:52:44 +0300

    better look

--- commands/Developer/github.js
+++ commands/Developer/github.js
@@ -27,15 +27,15 @@ class GitHub extends Command {
     const size = body.size < 1000 ? `${body.size} KB` : Math.floor(body.size / 1000) > 1000 ? `${(body.size / 1000 / 1000).toFixed(2)} GB` : `${(body.size / 1000).toFixed(2)} MB`;
     const license = body.license.name && body.license.url ? `[${body.license.name}](${body.license.url})` : body.license.name || "None";
     const footer = [];
-    if(body.fork) footer.push("**Forked**");
-    if(body.archived) footer.push("**Archived**");
+    if(body.fork) footer.push(`❯ **Forked** from [${body.parent.full_name}](${body.parent.html_url})`);
+    if(body.archived) footer.push("❯ This repository is **Archived**");
     const embed = new MessageEmbed()
       .setTitle(body.full_name)
       .setColor(0xFF0000)
       .setAuthor("GitHub", "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png")
       .setURL(body.html_url)
       .setThumbnail(body.owner.avatar_url)
-      .setDescription(`${body.description}\n\n❯ **Language:** ${body.language}\n❯ **Forks:** ${body.forks_count.toLocaleString()}\n❯ **License:** ${license}\n❯ **Open Issues:** ${body.open_issues.toLocaleString()}\n❯ **Watchers:** ${body.watchers_count.toLocaleString()}\n❯ **Stars:** ${body.stargazers_count.toLocaleString()}\n❯ **Clone Size:** ${size}${footer.length ? `\n\n${footer.join("|")}` : ""}`);
+      .setDescription(`${body.description}\n\n❯ **Language:** ${body.language}\n❯ **Forks:** ${body.forks_count.toLocaleString()}\n❯ **License:** ${license}\n❯ **Open Issues:** ${body.open_issues.toLocaleString()}\n❯ **Watchers:** ${body.watchers_count.toLocaleString()}\n❯ **Stars:** ${body.stargazers_count.toLocaleString()}\n❯ **Clone Size:** ${size}${footer.length ? footer.join("\n") : ""}`);
     return msg.send({ embed });
   }
 }
