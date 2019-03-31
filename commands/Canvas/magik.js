b5f73eb2c0fb99366cf0cae68682a42f62e188f4
pollen5 <mohammed.ahmed.minecraft@gmail.com>
2019-03-31 18:22:41 +0300

    allow url in magik

--- commands/Canvas/magik.js
+++ commands/Canvas/magik.js
@@ -6,14 +6,18 @@ class Magik extends Command {
   constructor(...args) {
     super(...args, {
       description: "Magik someone",
-      usage: "[user:username]",
+      usage: "[user:username|url:url]",
       cooldown: 5
     });
   }
 
   async run(msg, [user = msg.author]) {
+    const url = typeof user === "string" ? user : user.displayAvatarURL({
+      format: "png",
+      size: 2048
+    });
     const res = await get("https://nekobot.xyz/api/imagegen")
-      .query({ type: "magik", image: user.displayAvatarURL({ format: "png", size: 2048 }) });
+      .query({ type: "magik", image: url });
     const embed = new MessageEmbed()
       .setAuthor(user.tag, user.displayAvatarURL())
       .setColor(0xff0000)
