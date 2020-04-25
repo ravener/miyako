1c6df04e6fc85f9f401d36d171faa382daaf4d9b
pollen5 <mohammed.ahmed.minecraft@gmail.com>
2020-04-25 14:31:13 +0430

    oops

--- commands/utility/movie.js
+++ commands/utility/movie.js
@@ -1,4 +1,4 @@
-const { Command } = require("../../structures/Command.js");
+const Command = require("../../structures/Command.js");
 const { MessageEmbed } = require("discord.js");
 const fetch = require("node-fetch");
 
