0cc378910dcc98db9ce1b60670ba429b0bb44861
pollen5 <mohammed.ahmed.minecraft@gmail.com>
2020-04-25 16:09:49 +0430

    yo momma so fat

---
+++ commands/fun/yomomma.js
@@ -0,0 +1,24 @@
+const Command = require("../../structures/Command.js");
+const fetch = require("node-fetch");
+
+class YoMomma extends Command {
+  constructor(...args) {
+    super(...args, {
+      description: "Yo momma so fat.",
+      aliases: ["urmom"],
+      cooldown: 3,
+      usage: "yomomma [@user]"
+    });
+  }
+  
+  async run(ctx, [user]) {
+    user = await this.verifyUser(ctx, user, true);
+
+    const { joke } = await fetch("http://api.yomomma.info")
+      .then((res) => res.json());
+
+    return msg.send(`${user}, ${joke}`);
+  }
+}
+
+module.exports = YoMomma;
