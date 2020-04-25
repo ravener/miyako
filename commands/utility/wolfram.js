1b4334c4780a04cf1d737f760fb8638f0d035189
pollen5 <mohammed.ahmed.minecraft@gmail.com>
2020-04-25 13:44:00 +0430

    make wolfram more human like

--- commands/utility/wolfram.js
+++ commands/utility/wolfram.js
@@ -7,7 +7,7 @@ class Wolfram extends Command {
     super(...args, {
       description: "Query Wolfram Alpha with any mathematical question.",
       usage: "wolfram <query>",
-      aliases: ["what"],
+      aliases: ["what", "when", "where", "who", "why"],
       cooldown: 5
     });
   }
@@ -16,8 +16,11 @@ class Wolfram extends Command {
     if(!args.length) return ctx.reply("What are you trying to ask?");
     
     // Allow users to trigger this in a fancy way using @Miyako What time is it?
-    // If they invoke it with the "what" alias, we must also treat it as an argument.
-    const query = ctx.invokedName === "what" ? `What ${args.join(" ")}` : args.join(" ");
+    // If they invoke it with the "what"/"where"/"when"/"who"/"why" alias, we must also treat it as an argument.
+    // A trick to make it look like some advanced A.I bot i guess.
+    const query = ctx.invokedName === "what" || ctx.invokedName === "when" ||
+      ctx.invokedName === "where" || ctx.invokedName === "who" || ctx.invokedName === "why" ?
+      `${this.client.utils.toProperCase(ctx.invokedName)} ${args.join(" ")}` : args.join(" ");
 
     const url = new URL("http://api.wolframalpha.com/v2/query");
     url.search = new URLSearchParams([
