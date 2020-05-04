abac5eec13dbcfd3679af3c2f6e385219875abf8
pollen5 <mohammed.ahmed.minecraft@gmail.com>
2020-05-04 17:48:18 +0430

    rep and profile

--- commands/social/rep.js
+++ commands/social/rep.js
@@ -20,6 +20,7 @@ class Reputation extends Command {
     const reputation = user.settings.reputation + 1;
     await user.update({ reputation });
     await ctx.author.update({ repcooldown: new Date(ctx.message.createdTimestamp + 43200000) });
+    return ctx.reply(`Successfully gave a reputation point to ${user}`);
   }
 }
 
---
+++ commands/social/title.js
@@ -0,0 +1,19 @@
+const Command = require("../../structures/Command.js");
+
+class Title extends Command {
+  constructor(...args) {
+    super(...args, {
+      description: "Change your profile's title",
+      usage: "title <title>"
+    });
+  }
+
+  async run(ctx, [title]) {
+    if(!args.length) return ctx.reply("Baka! You must provide a title.");
+    const title = args.join(" ");
+    await ctx.author.update({ title });
+    return ctx.reply(`Success! changed your title view it with \`${msg.guild.settings.prefix}profile\``);
+  }
+}
+
+module.exports = Title;
