5cc46be968dcf28a74186da4ad21068f8d7d6e4d
pollen5 <mohammed.ahmed.minecraft@gmail.com>
2020-05-04 17:07:28 +0430

    settings

--- extensions/Guild.js
+++ extensions/Guild.js
@@ -8,4 +8,14 @@ module.exports = Structures.extend("Guild", (Guild) => class MiyakoGuild extends
   syncSettings() {
     return this.client.settings.sync(this.id);
   }
+
+  /**
+   * Alias
+   * this.client.settings.update(guild.id, { prefix: "..." }) 
+   * to just
+   * guild.update({ prefix: "..." })
+   */
+  update(obj) {
+    return this.client.settings.update(this.id, obj);
+  }
 });
--- extensions/GuildMember.js
+++ extensions/GuildMember.js
@@ -2,9 +2,19 @@ const { Structures } = require("discord.js");
 
 module.exports = Structures.extend("GuildMember", (GuildMember) => class MiyakoGuildMember extends GuildMember {
 
+  /**
+   * Alias
+   * this.client.members.update(`${guild.id}.${member.id}`, { points: 0 })
+   * to just
+   * member.update({ points: 0 })
+   */
+  update(obj) {
+    return this.client.members.update(`${this.guild.id}.${this.id}`, obj);
+  }
+
   get settings() {
     const id = `${this.guild.id}.${this.id}`;
-    return this.client.members.get(`${this.guild.id}.${this.id}`) || { id, level: 0, points: 0, daily: null };
+    return this.client.members.get(id) || { id, level: 0, points: 0, daily: null };
   }
 
   syncSettings() {
@@ -17,15 +27,13 @@ module.exports = Structures.extend("GuildMember", (GuildMember) => class MiyakoG
 
   setLevel(level) {
     if(isNaN(level)) throw new Error("Level cannot be NaN");
-    const id = `${this.guild.id}.${this.id}`;
-    return this.client.members.update(id, { level });
+    return this.update({ level });
   }
 
   async givePoints(amount) {
-    const id = `${this.guild.id}.${this.id}`;
     const points = parseInt(this.settings.points) + parseInt(amount);
     if(isNaN(points)) throw new Error("Cannot give NaN points to member.");
-    return this.client.members.update(id, { points });
+    return this.update({ points });
   }
 
   takePoints(amount) {
