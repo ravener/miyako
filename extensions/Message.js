a4c8233231e04078d20cea06d2a0d0671634a494
pollen5 <mohammed.ahmed.minecraft@gmail.com>
2020-05-04 16:40:36 +0430

    MiyakoMessage#member

--- extensions/Message.js
+++ extensions/Message.js
@@ -2,6 +2,11 @@ const { Structures } = require("discord.js");
 
 module.exports = Structures.extend("Message", (Message) => class MiyakoMessage extends Message {
 
+  get member() {
+    if(this.guild) return super.member;
+    return { user: this.author, displayName: this.author.username };
+  }
+
   async awaitReply(question, filter, limit = 60000, embed) {
     await this.channel.send(question, embed);
 
