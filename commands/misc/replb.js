5e07af61f19a707b81f009ce1c4a420c8066c2a8
pollen5 <mohammed.ahmed.minecraft@gmail.com>
2020-05-05 14:32:29 +0430

    fix

--- commands/misc/replb.js
+++ commands/misc/replb.js
@@ -17,12 +17,16 @@ class RepLB extends Command {
       .setColor(0x9590EE)
       .setTitle(`Top${rows.length === 1 ? "" : ` ${rows.length}`} respected user${rows.length > 1 ? "s" : ""} by reputations`)
       .setAuthor(ctx.author.tag, ctx.author.displayAvatarURL({ size: 64 }));
+
+    const lb = [];
     
     for(let i = 0; i < rows.length; i++) {
       const row = rows[i];
       const user = await this.client.users.fetch(row.id);
-      embed.addField(`${((i + 1).toString()).padEnd(2, " ")} ❯ ${user.tag} - ${row.reputation}`);
+      lb.push(`${((i + 1).toString()).padEnd(2, " ")} ❯ ${user.tag} - ${row.reputation}`);
     }
+
+    embed.setDescription(lb.join("\n"));
 
     return ctx.reply({ embed });
   }
