b1970e35d9bb00d771e7238f1c1fb3bf1db44713
pollen5 <mohammed.ahmed.minecraft@gmail.com>
2020-05-05 17:15:53 +0430

    use client.setInterval

--- events/miyakoReady.js
+++ events/miyakoReady.js
@@ -4,7 +4,7 @@ class MiyakoReady extends Event {
   async run() {
     this.client.user.setActivity(`@${this.client.user.username} help | ${this.client.guilds.cache.size} Server${this.client.guilds.cache.size > 1 ? "s" : ""}!`);
     this.client.sweeper.run();
-    this.client.sweeper.task = setInterval(() => this.client.sweeper.run(), 600000);
+    this.client.sweeper.task = this.client.setInterval(() => this.client.sweeper.run(), 600000);
   } 
 }
 
