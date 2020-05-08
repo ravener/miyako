b53ed2629eef5b1243e0564b7d10243125685bed
pollen5 <mohammed.ahmed.minecraft@gmail.com>
2020-05-08 21:17:32 +0430

    fix

--- commands/utility/quote.js
+++ commands/utility/quote.js
@@ -1,4 +1,4 @@
-const { Command } = require("../../structures/Command.js");
+const Command = require("../../structures/Command.js");
 const { MessageEmbed } = require("discord.js");
 
 class Quote extends Command {
