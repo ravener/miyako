c93950c0a445e1e60b35caed5bdf77bfcd20b004
Raven Ravener <ravener.anime@gmail.com>
2022-11-20 20:58:10 +0430

    feat: deploy command

---
+++ src/commands/owner/deploy.js
@@ -0,0 +1,30 @@
+const Command = require("../../structures/Command.js");
+
+class Deploy extends Command {
+  constructor(...args) {
+    super(...args, {
+      description: "Deploys slash commands.",
+      ownerOnly: true,
+      modes: ["text"] 
+    });
+  }
+
+  async run(ctx) {
+    try {
+      const body = [];
+
+      for (const command of this.client.commands.values()) {
+        if (command.modes.includes("slash") && command.enabled) {
+          body.push(command.getSlashCommandData().toJSON());
+        }
+      }
+
+      await this.client.application.commands.set(body);
+      return ctx.reply(`Successfully registered ${body.length} application commands.`);
+    } catch (err) {
+      return ctx.reply(`There was an error registering commands: \`${err.message}\``);
+    }
+  }
+}
+
+module.exports = Deploy;
