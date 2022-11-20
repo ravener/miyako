e5bcea5e4bcead7cad0cae5010c6998a62348b12
Raven Ravener <ravener.anime@gmail.com>
2022-11-20 22:37:04 +0430

    feat: yomomma command

---
+++ src/commands/fun/yomomma.js
@@ -0,0 +1,31 @@
+const Command = require("../../structures/Command.js");
+const { request } = require("undici");
+
+class YoMomma extends Command {
+  constructor(...args) {
+    super(...args, {
+      description: "Yo momma so fat.",
+      aliases: ["urmom"],
+      cooldown: 3,
+      usage: "yomomma [@user]",
+      options: [
+        {
+          name: "user",
+          description: "The user to insult their mom. By default that's you!",
+          type: "user"
+        }
+      ]
+    });
+  }
+  
+  async run(ctx, options) {
+    const user = options.getUser("user") ?? ctx.author;
+
+    const { joke } = await fetch("http://api.yomomma.info")
+      .then(({ body }) => body.json());
+
+    return ctx.reply(`${user}, ${joke}`);
+  }
+}
+
+module.exports = YoMomma;
