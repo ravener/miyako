e37c0a3b550ca0586e1a681cc8f308ed735670d7
Raven Ravener <ravener.anime@gmail.com>
2022-11-20 14:23:07 +0430

    fix: messageDelete

--- src/events/messageDelete.js
+++ src/events/messageDelete.js
@@ -2,10 +2,9 @@ const Event = require("../structures/Event.js");
 
 class MessageDelete extends Event {
   async run(message) {
-    const ctx = this.client.commands.contexts.get(message.id);
-    if (!ctx || !ctx.lastReply || !ctx.lastReply.deletable) return;
-
-    return ctx.lastReply.delete().catch(() => null);
+    if (message.lastReply && message.lastReply.deletable) {
+      return message.lastReply.delete().catch(() => null);
+    }
   }
 }
 
