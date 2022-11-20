d6c37b634b42d069cfea2ca97c917b78400704e9
Raven Ravener <ravener.anime@gmail.com>
2022-11-20 22:48:58 +0430

    feat: report event errors

--- src/events/eventError.js
+++ src/events/eventError.js
@@ -1,8 +1,17 @@
 const Event = require("../structures/Event.js");
 
 class EventError extends Event {
-  run(event, err) {
+  async run(event, err) {
     this.client.log.error(err);
+
+    const channel = this.client.channels.cache.get("454776836929617921");
+    if (!channel) return;
+
+    const embed = this.client.embed()
+      .setTitle("Event Error")
+      .setDescription(`An Error occured in event: ${event.name}\n\`\`\`js\n${err.stack || err}\`\`\``);
+
+    return channel.send({ embeds: [embed] }).catch(() => null);
   }
 }
 
