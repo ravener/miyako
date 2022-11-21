9704b66345908ea3a31e5112d658482497bcba81
Raven Ravener <ravener.anime@gmail.com>
2022-11-21 19:21:55 +0430

    feat: wolfram command

---
+++ src/commands/utility/wolfram.js
@@ -0,0 +1,51 @@
+const Command = require("../../structures/Command.js");
+const { request } = require("undici");
+const { toProperCase } = require("../../utils/utils.js");
+
+class Wolfram extends Command {
+  constructor(...args) {
+    super(...args, {
+      description: "Query Wolfram Alpha with any mathematical question.",
+      usage: "wolfram <query>",
+      aliases: ["what", "when", "where", "who", "why", "how"],
+      cooldown: 5,
+      options: [
+        {
+          name: "query",
+          description: "The query to ask wolfram about.",
+          type: "string",
+          required: true
+        }
+      ]
+    });
+  }
+  
+  async run(ctx, options) {
+    // Allow users to trigger this in a fancy way using @Miyako What time is it?
+    // If they invoke it with the "what"/"where"/"when"/"who"/"why" alias, we must also treat it as an argument.
+    // A trick to make it look like some advanced A.I bot i guess.
+    const query = ["what", "where", "when", "how", "who", "why"].includes(ctx.alias) ? `${toProperCase(ctx.alias)} ${options.getString("query")}` : options.getString("query");
+
+    const url = new URL("http://api.wolframalpha.com/v2/query");
+    url.search = new URLSearchParams([
+      ["input", query],
+      ["primary", true],
+      ["appid", process.env.WOLFRAM],
+      ["output", "json"]
+    ]);
+    
+    const pods = await request(url)
+      .then(({ body }) => body.json())
+      .then((body) => body.queryresult.pods);
+    
+    if (!pods || pods.error) return ctx.reply("Couldn't find an answer to that question!");
+
+    const embed = this.client.embed()
+      .setTitle(pods[0].subpods[0].plaintext)
+      .setDescription(pods[1].subpods[0].plaintext.substring(0, 1980));
+
+    return ctx.reply({ embeds: [embed] });
+  }
+}
+
+module.exports = Wolfram;
