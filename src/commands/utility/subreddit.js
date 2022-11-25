8b9505e1d05535ba4663947330c556d4eda3ab17
Raven Ravener <ravener.anime@gmail.com>
2022-11-25 14:04:43 +0430

    feat: subreddit command

---
+++ src/commands/utility/subreddit.js
@@ -0,0 +1,55 @@
+const Command = require("../../structures/Command.js");
+const { request } = require("undici");
+
+class SubReddit extends Command {
+  constructor(...args) {
+    super(...args, {
+      aliases: ["sub", "reddit"],
+      description: "Returns information on a subreddit.",
+      usage: "subreddit <name>",
+      cooldown: 5,
+      options: [
+        {
+          name: "subreddit",
+          description: "The subreddit to view",
+          type: "string",
+          required: true
+        }
+      ]
+    });
+
+    this.errorMessage = "There was an error. Reddit may be down, or the subreddit doesnt exist.";
+  }
+
+  async run(ctx, options) {
+    const subredditName = options.getString("subreddit");
+    const subreddit = await request(`https://www.reddit.com/r/${subredditName}/about.json`)
+      .then(({ body }) => body.json())
+      .then((body) => {
+        if (body.kind === "t5") return body.data;
+        throw "That subreddit doesn't exist.";
+      })
+      .catch(() => { throw this.errorMessage; });
+
+    const embed = this.client.embed()
+      .setTitle(subreddit.title)
+      .setDescription(subreddit.public_description)
+      .setURL(`https://www.reddit.com/r/${subredditName}/`)
+      .setThumbnail(subreddit.icon_img)
+      .setImage(subreddit.banner_img)
+      .addFields({
+        name: "Subscribers",
+        value: subreddit.subscribers.toLocaleString(),
+        inline: true
+      })
+      .addFields({
+        name: "Users Active",
+        value: subreddit.accounts_active.toLocaleString(),
+        inline: true
+      });
+
+    return ctx.reply({ embeds: [embed] });
+  }
+}
+
+module.exports = SubReddit;
